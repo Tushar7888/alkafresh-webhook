79ca70394bee271e6a0f31f7dce3b0f10d061e0a
Tushar7888 <alkafreshbot@gmail.com>
2025-06-29 23:10:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,7 @@ def webhook():
         msg.body("✅ Flask is live. Type 'demo' for video link.")
     
     return str(resp)
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
